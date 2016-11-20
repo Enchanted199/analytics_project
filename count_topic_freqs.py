6364bc5c2d416940e19ace5fa833152cc2aedf60
Brian Buxton <bbux10@gmail.com>
2016-11-20 13:02:50 -0500

    Pulled out text processing into one script, modified other scripts to use this one

--- count_topic_freqs.py
+++ count_topic_freqs.py
@@ -5,55 +5,13 @@ Created on Mon Nov 14 18:47:15 2016
 @author: bbux
 """
 
-from nltk.tokenize import RegexpTokenizer
-from stop_words import get_stop_words
-from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models
 from operator import itemgetter
 import gensim
 import pandas as pd
 import os
-import re
 import argparse
-
-def clean_essay(string):
-    string = re.sub(r"\\t", " ", string)   
-    string = re.sub(r"\\n", " ", string)   
-    string = re.sub(r"\\r", " ", string)   
-    string = re.sub(r"[^A-Za-z0-9\']", " ", string)   
-    string = re.sub(r"\s{2,}", " ", string)
-    string = string.lower()
-    return string.strip()
-
-def prep_data(df, text_vars):
-    for var in text_vars:
-            df[var][pd.isnull(df[var])] = ""
-            df[var] = df[var].apply(clean_essay)
-
-def create_texts(df, text_vars, stem=False):
-    pattern = r"(?u)\b[A-Za-z0-9()\'\-?!\"%]+\b"
-    tokenizer = RegexpTokenizer(pattern)
-    en_stop = get_stop_words('en')
-    p_stemmer = PorterStemmer()
-    
-    texts = []
-    for index, row in df.iterrows():
-        all_tokens = []
-        for var in text_vars:
-            tokens = tokenizer.tokenize(row[var])
-        
-            # remove stop words from tokens
-            tokens = [i for i in tokens if not i in en_stop]
-            
-            # stem tokens
-            if stem:
-                tokens = [p_stemmer.stem(i) for i in tokens]
-        
-            all_tokens += tokens
-        # add tokens to list
-        texts.append(all_tokens)
-    
-    return texts
+import text_util
 
 
 """
@@ -65,14 +23,14 @@ def main(args):
 
     df = pd.read_csv(args.inputfile)
     print("preparing data...")
-    prep_data(df, args.fields)
+    text_util.prep_data(df, args.fields)
     
     print("Creating text and loading dictionary for " + str(args.fields) + "...")
     dictionary = corpora.Dictionary().load_from_text(args.dictfile)
     
     ldamodel = gensim.models.ldamodel.LdaModel.load(args.modelfile)
     
-    texts = create_texts(df, args.fields, args.dostem)
+    texts = text_util.create_texts(df, args.fields, args.dostem)
     counts = {}
     for text in texts:
         doc = dictionary.doc2bow(text)
--- inspect_docs.py
+++ inspect_docs.py
@@ -5,56 +5,14 @@ Created on Mon Nov 14 18:47:15 2016
 @author: bbux
 """
 
-from nltk.tokenize import RegexpTokenizer
-from stop_words import get_stop_words
-from nltk.stem.porter import PorterStemmer
+
 from gensim import corpora, models
 from operator import itemgetter
 import gensim
 import pandas as pd
 import os
-import re
 import argparse
-
-def clean_essay(string):
-    string = re.sub(r"\\t", " ", string)   
-    string = re.sub(r"\\n", " ", string)   
-    string = re.sub(r"\\r", " ", string)   
-    string = re.sub(r"[^A-Za-z0-9\']", " ", string)   
-    string = re.sub(r"\s{2,}", " ", string)
-    string = string.lower()
-    return string.strip()
-
-def prep_data(df, text_vars):
-    for var in text_vars:
-            df[var][pd.isnull(df[var])] = ""
-            df[var] = df[var].apply(clean_essay)
-
-def create_texts(df, text_vars, stem=False):
-    pattern = r"(?u)\b[A-Za-z0-9()\'\-?!\"%]+\b"
-    tokenizer = RegexpTokenizer(pattern)
-    en_stop = get_stop_words('en')
-    p_stemmer = PorterStemmer()
-    
-    texts = []
-    for index, row in df.iterrows():
-        all_tokens = []
-        for var in text_vars:
-            tokens = tokenizer.tokenize(row[var])
-        
-            # remove stop words from tokens
-            tokens = [i for i in tokens if not i in en_stop]
-            
-            # stem tokens
-            if stem:
-                tokens = [p_stemmer.stem(i) for i in tokens]
-        
-            all_tokens += tokens
-        # add tokens to list
-        texts.append(all_tokens)
-    
-    return texts
-
+import text_util
 
 """
 Start Script
@@ -65,20 +23,22 @@ def main(args):
 
     df = pd.read_csv(args.inputfile)
     print("preparing data...")
-    prep_data(df, args.fields)
+    text_util.prep_data(df, args.fields)
     
     print("Creating text and loading dictionary for " + str(args.fields) + "...")
     dictionary = corpora.Dictionary().load_from_text(args.dictfile)
     
     ldamodel = gensim.models.ldamodel.LdaModel.load(args.modelfile)
     
-    texts = create_texts(df, args.fields, args.dostem)
+    texts = text_util.create_texts(df, args.fields, args.dostem)
     for text in texts[0:10]:
         doc_topics = ldamodel[dictionary.doc2bow(text)]
-        _srtd = sorted(doc_topics, key=itemgetter(1), reverse=1)
+        srtd = sorted(doc_topics, key=itemgetter(1), reverse=1)
         print(text)
-        top, score = _srtd[0]
-        print(ldamodel.print_topic(top))
+        size = 3 if len(srtd) > 3 else len(srtd)
+        for topic_id, score in srtd[0:size]:
+            print(str(score))
+            print(ldamodel.print_topic(topic_id))
         print("--------------------------------")
         
 
