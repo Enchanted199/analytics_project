187d00113d54a59c75519f30f0e728e1cd034e4b
Brian Buxton <bbux10@gmail.com>
2016-11-22 19:13:41 -0500

    fixed up count topic freqs and print topics

--- count_topic_freqs.py
+++ count_topic_freqs.py
@@ -5,53 +5,28 @@ Created on Mon Nov 14 18:47:15 2016
 @author: bbux
 """
 
-from gensim import corpora, models
-from operator import itemgetter
-import gensim
 import pandas as pd
 import os
-import argparse
-import text_util
-
+import operator
+import sys
 
 """
 Start Script
 """
-def main(args):
-    if not os.path.exists(args.inputfile):
-        raise Exception("Input file does not exist!: " + args.inputfile)
+inputfile = sys.argv[1]
+if not os.path.exists(inputfile):
+    raise Exception("Input file does not exist!: " + inputfile)
 
-    df = pd.read_csv(args.inputfile)
-    print("preparing data...")
-    text_util.prep_data(df, args.fields)
-    
-    print("Creating text and loading dictionary for " + str(args.fields) + "...")
-    dictionary = corpora.Dictionary().load_from_text(args.dictfile)
-    
-    ldamodel = gensim.models.ldamodel.LdaModel.load(args.modelfile)
-    
-    texts = text_util.create_texts(df, args.fields, args.dostem)
-    counts = {}
-    for text in texts:
-        doc = dictionary.doc2bow(text)
-        doc_topics = ldamodel[doc]
-        _srtd = sorted(doc_topics, key=itemgetter(1), reverse=1)
-        top, score = _srtd[0]
-        if top in counts:
-            counts[top] += 1
-        else:
-            counts[top] = 1
-            
-    print(str(counts))
+df = pd.read_csv(inputfile, index_col=0)
+df = df.drop('projectid', 1)
+counts = {}
+for col in df.idxmax(axis=1):
+    if col in counts:
+        counts[col] += 1
+    else:
+        counts[col] = 1
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--inputfile", help="input data file", required=True)
-    parser.add_argument("-m", "--modelfile", help="input lda model file", required=True)
-    parser.add_argument("-d", "--dictfile", help="input dictonary file", required=True)
-    parser.add_argument("-f", "--fields", nargs='+', help="fields to generate models for", required=True)
-    parser.add_argument("--dostem", help="should terms be stemmed, default is no stemming",
-                        required=False, action="store_true", default=False)
-    args = parser.parse_args()
-    main(args)
-    
+for k, v in sorted(counts.items(), key=operator.itemgetter(1)):
+    print("%s, %d" % (k, v))
+
+   
--- print_topics.py
+++ print_topics.py
@@ -5,11 +5,14 @@ Created on Sat Nov 19 16:59:51 2016
 @author: bbux
 """
 import sys
-
+import re
 import gensim
 
 if __name__ == "__main__":
     file = sys.argv[1]
     n = int(sys.argv[2])
     ldamodel = gensim.models.ldamodel.LdaModel.load(file)
-    print(ldamodel.print_topics(num_topics=n, num_words=10))
+    topics = ldamodel.print_topics(n, num_words=10)
+    for tid, topic in topics:
+        terms = sorted(re.findall(r'"(.*?)"', topic))
+        print(str(tid) + "," + ",".join(terms))
