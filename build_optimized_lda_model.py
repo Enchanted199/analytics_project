1e207e1ef3486bcad180cab30ce4ee03e5303a3b
Brian Buxton <bbux10@gmail.com>
2016-12-04 14:20:47 -0500

    added stdout flushing

--- build_optimized_lda_model.py
+++ build_optimized_lda_model.py
@@ -14,6 +14,11 @@ import text_util
 import sys
 import convert_to_features
 
+
+def log(message):
+    print(message)
+    sys.stdout.flush()
+    
 """
 Start Script
 """
@@ -27,25 +32,25 @@ def main(args):
     start = time.time()
     df = pd.read_csv(args.inputfile)
 
-    print("preparing data...")
+    log("preparing data...")
     text_util.prep_data(df, args.fields)
-    print("time: " + str(time.time() - start))
-    sys.stdout.flush()
+    log("time: " + str(time.time() - start))
     
-    print("Creating text and dictionary for " + str(args.fields) + "...")
+    
+    log("Creating text and dictionary for " + str(args.fields) + "...")
     dictionary = text_util.create_iterative_dictionary(df, args.fields, args.dostem,
                                                        args.no_below, args.no_above)
-    print("time: " + str(time.time() - start))
-    sys.stdout.flush()
+    log("time: " + str(time.time() - start))
+    
     
     dict_file_name = args.outdir + "/" + args.prefix + '-opt-dict.txt'
-    print("Saving dictionary to " + dict_file_name)
+    log("Saving dictionary to " + dict_file_name)
     dictionary.save_as_text(dict_file_name)
-    print("time: " + str(time.time() - start))
-    sys.stdout.flush()
+    log("time: " + str(time.time() - start))
+    
     
     tokenizer, en_stop, stemmer = text_util.get_tokenizer_tools()
-    print("Createing corpus for " + str(args.fields) + "...")
+    log("Createing corpus for " + str(args.fields) + "...")
     
     # convert tokenized documents into a document-term matrix
     corpus = []
@@ -55,38 +60,42 @@ def main(args):
         corpus.append(dictionary.doc2bow(all_tokens))
         ids.append(row['projectid'])
         if len(corpus) % 1000 == 0:
-            print("time: " + str(time.time() - start) + " count: " + str(len(corpus)))
-            sys.stdout.flush()
+            log("time: " + str(time.time() - start) + " count: " + str(len(corpus)))
+            
 
-    print("Generating ldamodel for " + str(args.fields) + "...")
+    log("Generating ldamodel for " + str(args.fields) + "...")
     # generate LDA model
     ldamodel = gensim.models.ldamodel.LdaModel(corpus=corpus, num_topics=args.num_topics, 
                                                id2word = dictionary, passes=args.passes)
 
-    if args.printmodel:
-        print(ldamodel.print_topics(num_topics=args.num_topics, num_words=10))
+    if args.logmodel:
+        log(ldamodel.log_topics(num_topics=args.num_topics, num_words=10))
 
     lda_model_file = args.outdir + "/" + args.prefix + '-opt-lda.model'
-    print("Saving ldamodel to " + lda_model_file)
+    log("Saving ldamodel to " + lda_model_file)
     ldamodel.save(lda_model_file)
-    print("Total processing time " + str(time.time() - start) + " for " + str(args.fields))
+    log("Total processing time " + str(time.time() - start) + " for " + str(args.fields))
+    
     if args.convertinput:
-        print("Converting document vectors to topic features...")
+        log("Converting document vectors to topic features...")
+        
         topic_csv = args.outdir + "/" + args.prefix + '-topics.csv'
         convert_to_features.convert(list(zip(ids, corpus)), ldamodel, topic_csv)
+        log("time: " + str(time.time() - start))
+        
 
-    print_config(args)
+    log_config(args)
 
 
-def print_config(args):
-    print("Input: " + args.inputfile)
-    print("Output: " + args.outdir)
-    print("Model Settings: ")
-    print("  Do Stem?:" + str(args.dostem))
-    print("  No Above: " + str(args.no_above))
-    print("  No Below: " + str(args.no_below))
-    print("  Num Topics: " + str(args.num_topics))
-    print("  Passes: " + str(args.passes))
+def log_config(args):
+    log("Input: " + args.inputfile)
+    log("Output: " + args.outdir)
+    log("Model Settings: ")
+    log("  Do Stem?:" + str(args.dostem))
+    log("  No Above: " + str(args.no_above))
+    log("  No Below: " + str(args.no_below))
+    log("  Num Topics: " + str(args.num_topics))
+    log("  Passes: " + str(args.passes))
         
 
 if __name__ == "__main__":
@@ -95,7 +104,7 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--outdir", help="where to store results", required=True)
     parser.add_argument("-f", "--fields", nargs='+', help="fields to generate models for", required=True)
     parser.add_argument("-p", "--prefix", default="text", help="prefix for naming saved data", required=False)
-    parser.add_argument("--printmodel", help="print out the topics", required=False, action="store_true")
+    parser.add_argument("--logmodel", help="print out the topics", required=False, action="store_true")
     parser.add_argument("--dostem", help="should terms be stemmed, default is no stemming",
                         required=False, action="store_true", default=False)
     parser.add_argument("--convertinput", help="should csv with topic features be created from input documents",
--- evaluate_topics.py
+++ evaluate_topics.py
@@ -6,50 +6,23 @@ import os
 import sys
 import pandas as pd
 
-from sklearn import tree
 from sklearn import linear_model
-
-from sklearn.cross_validation import train_test_split
 from sklearn import cross_validation
-
 from sklearn import metrics
+
 import matplotlib.pyplot as plt
+import time
 
-
-folder = sys.argv[1]
-print("Loading files...")
-outcomes_df = pd.read_csv(os.path.join(folder, "outcomes.csv"))
-topics_df = pd.read_csv(os.path.join(folder, "topics.csv"))
-   
-df = pd.merge(outcomes_df, topics_df, how = 'left', on = 'projectid')
-
-df["y"] = df["is_exciting"].apply(lambda x: 1 if x == "t" else 0)
-df = df.fillna(0)
-
-target = df.y
-cols = [ 'topic' + str(j) for j in range(100)]
-predictors = df[cols]
-
-x_train, x_test, y_train, y_test = train_test_split(
-    predictors, target, test_size=0.33, random_state=52)
-
-logr_model = linear_model.LogisticRegression()
-logr_model.fit(x_train, y_train)
-logr_predictions = logr_model.predict(x_test)
-                                 
-dt_model = tree.DecisionTreeClassifier('entropy')
-dt_model.fit(x_train, y_train)
-dt_predictions = dt_model.predict(x_test)
 
 def cross_score(model, predictors, target):
     scores = cross_validation.cross_val_score(model, predictors, 
                                  target, cv=5, scoring='roc_auc')
-    print("ROC AUC : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    log("ROC AUC : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
     
 
 
-def print_metrics(y_test, predictions):
-    print(metrics.accuracy_score(y_test, predictions))
+def log_metrics(y_test, predictions):
+    log(metrics.accuracy_score(y_test, predictions))
 
 
 def plot_roc(fpr, tpr, roc_auc, label):
@@ -64,7 +37,7 @@ def plot_roc(fpr, tpr, roc_auc, label):
     plt.xlabel('False Positive Rate')
     plt.show()
 
-def run_model_and_plot_roc(model, x_train, y_train, x_test, label):
+def run_model_and_plot_roc(model, x_train, y_train, x_test, y_test, label):
     model.fit(x_train, y_train)
     predict = model.predict_proba(x_test)
     
@@ -74,19 +47,39 @@ def run_model_and_plot_roc(model, x_train, y_train, x_test, label):
     plot_roc(fpr, tpr, roc_auc, label)
     return predict
 
+def log(message):
+    print(message)
+    sys.stdout.flush()
+
+folder = sys.argv[1]
+topics = sys.argv[2]
+start = time.time()
+log("Loading files...")
+
+outcomes_df = pd.read_csv(os.path.join(folder, "outcomes.csv"))
+topics_df = pd.read_csv(topics)
+   
+df = pd.merge(outcomes_df, topics_df, how = 'left', on = 'projectid')
+
+df["y"] = df["is_exciting"].apply(lambda x: 1 if x == "t" else 0)
+df = df.fillna(0)
+
+target = df.y
+cols = [ 'topic' + str(j) for j in range(100)]
+predictors = df[cols]
+
+log("Creating LogisticRegression Model...")
 logr_model = linear_model.LogisticRegression()
-logr_predict = run_model_and_plot_roc(logr_model, x_train, y_train, x_test, 'Linear Regression')
 
-tree_model = tree.DecisionTreeClassifier('entropy')
-tree_predict = run_model_and_plot_roc(tree_model, x_train, y_train, x_test, 'Decision Tree')
-
+log("Creating SGD Model...")
 sdg_model = linear_model.SGDClassifier(penalty="l2",loss="log",fit_intercept=True,
                                        shuffle=True,n_iter=20, n_jobs=-1,alpha=0.000005)
-sdg_predict = run_model_and_plot_roc(sdg_model, x_train, y_train, x_test, 'SGD')
 
+log("Running Cross Validation on Logr Model...")
 cross_score(logr_model, predictors, target)
-cross_score(dt_model, predictors, target)
+
+log("Running Cross Validation on SGD Model...")
 cross_score(sdg_model, predictors, target)
 
-#print_metrics(y_test, logr_predictions)
-#print_metrics(y_test, dt_predictions)
+log("Total processing time " + str(time.time() - start))
+
